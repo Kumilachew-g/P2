4292caeb9edded4839d1e33530d9c0b341bb54b1
Kumilachew-g <kumilachew1212@gmail.com>
2023-02-06 18:16:06 +0300

    Update

--- js/projects.js
+++ js/projects.js
@@ -3,7 +3,7 @@ const projects = [
     name: 'Hotelator',
     description:
       'Hotelator is a web application that allows users to book hotel rooms and view the status of their reservations.',
-    featuredImage: './images/hotelator.png',
+    featuredImage: 'images/hotelator.png',
     technologies: [
       'Ruby',
       'Ruby on Rails',
@@ -19,7 +19,7 @@ const projects = [
     name: 'Budget App',
     description:
       'The budget app is a mobile web application where you can manage your budget: you have a list of transactions associated with a category so that you can see how much money you spent and on what.',
-    featuredImage: './images/budget4.png',
+    featuredImage: 'images/budget4.png',
     technologies: ['Ruby', 'Ruby on Rails', 'Postgres', 'Tailwind CSS'],
     liveVersion: 'https://budget-app-lfai.onrender.com/',
     source: 'https://github.com/Kumilachew-g/budget-app',
@@ -28,7 +28,7 @@ const projects = [
     name: 'PokeDex App',
     description:
       'This project is a PokeDex featuring the original Pokemon from Gen1. The project was built with react, redux, and fetch API.',
-    featuredImage: './images/poke.png',
+    featuredImage: 'images/poke.png',
     technologies: ['React', 'Redux', 'CSS', 'API'],
     liveVersion: 'https://pekedex.netlify.app/',
     source: 'https://github.com/Kumilachew-g/react-capstone',
@@ -46,7 +46,7 @@ const projects = [
     name: 'Recipe App',
     description:
       'The Recipe App keeps track of all our recipes, ingredients, and inventory.',
-    featuredImage: './images/recipe2.png',
+    featuredImage: 'images/recipe2.png',
     technologies: ['Rails', 'CSS', 'Bootstrap', 'Ruby on Rails ', 'PostgreSQL'],
     liveVersion: 'https://recipe-app-7d79.onrender.com',
     source: 'https://github.com/wandergithub/Recipe-app',
@@ -55,7 +55,7 @@ const projects = [
     name: 'Tech Conference Website',
     description:
       'Website about tech company built with HTML, CSS, and Javascript',
-    featuredImage: './images/capstone.png',
+    featuredImage: 'images/capstone.png',
     technologies: ['html', 'css', 'javascript', 'bootstrap'],
     liveVersion: 'https://kumilachew-g.github.io/capstone-project-one/',
     source: 'https://github.com/Kumilachew-g/capstone-project-one',
@@ -63,7 +63,7 @@ const projects = [
   {
     name: 'Awesome Books',
     description: 'Basic website for add and remove books',
-    featuredImage: './images/awesomee.png',
+    featuredImage: 'images/awesomee.png',
     technologies: ['HTML', 'CSS', 'JavaScript', 'Bootstrap'],
     liveVersion: 'https://kumilachew-g.github.io/Awesome-Books-SPA/',
     source: 'https://github.com/Kumilachew-g/Awesome-Books-SPA',
@@ -71,7 +71,7 @@ const projects = [
   {
     name: 'ToDo List',
     description: 'This is a website for managing the daily tasks',
-    featuredImage: './images/todo.png',
+    featuredImage: 'images/todo.png',
     technologies: ['HTML', 'CSS', 'JavaScript', 'WebPacks', 'Jest'],
     liveVersion: 'https://kumilachew-g.github.io/To-Do-List/dist',
     source: 'https://github.com/Kumilachew-g/To-Do-List',
